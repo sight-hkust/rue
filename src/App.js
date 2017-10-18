31c481ec7d79ab6a21075cc7fbdb686649ef960c
ckylee <ckylee@connect.ust.hk>
2017-10-18 17:34:14 +0800

    resolved conflict
    changes made to App.js was in conflict. Committing to save progress and
    migrate to linux environment.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+/*eslint-disable no-unused-vars*/
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { ConnectedRouter } from 'react-router-redux'
-
+/*eslint-enable no-unused-vars*/
 
 /*
   Import all the relavant pages from the
   scenes directory
 */
+<<<<<<< HEAD
 import Home from './scenes/Home'
 import Login from './scenes/Login'
 import Registration from './scenes/Registration'
 import PatientList from './scenes/PatientList'
+=======
+import Root from 'scenes/Root'
+import Home from 'scenes/Home'
+import Login from 'scenes/Login'
+import Registration from 'scenes/Registration'
+>>>>>>> refs/remotes/origin/develop
 
 /*
   The exported App component act as both a router
@@ -22,7 +30,8 @@ class App extends Component {
     return (
       <ConnectedRouter history={this.props.history}>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Root} />
+          <Route path="/dashboard" component={Home}/>
           <Route path="/login" component={Login} />
           <Route path="/register" component={Registration} />
           <Route path="/feature-patientlist" component={PatientList} />
