e697f03fcf54c8282b4f482966cb034b2d628df7
codingcarrot <tiffanymak1996@gmail.com>
2017-10-28 21:23:03 +0800

    Added Physical Examination code

---
+++ src/scenes/Physical Examination/index.js
@@ -0,0 +1,45 @@
+/*eslint-disable no-unused-vars*/
+import React, { Component } from 'react'
+import { Layout } from 'antd'
+import NavigationBar from '../../components/NavigationBar'
+import './style.css'
+/*eslint-enable no-unused-vars*/
+class PhysicalExamination extends Component {
+  render(){
+    const { Header, Content, Footer } = Layout
+    return (
+      <div>
+        <Layout>
+          <NavigationBar></NavigationBar>
+          <Layout style={{ marginLeft: 250 }}>
+            <Header style={{ background: '#8A91FF', padding: 0 }} />
+            <Content style={{ margin: '24px 16px 0', overflow: 'initial', display:'flex' }}>
+              <div className="em-demo-edit-fields">
+                <center style={{marginTop: '20%'}}>
+                  <phyExForm></phyExForm>
+                </center>
+              </div>
+              <div className="em-demo-queue">
+                <section>
+                  <center style={{marginTop: '20%'}}>
+                    <h2>Patient Queue</h2>
+                  </center>
+                </section>
+              </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+            SIGHT ©2017 Hong Kong University of Science and Technology
+            </Footer>
+          </Layout>
+        </Layout>
+      </div>
+    )
+  }
+}
+
+export default PhysicalExamination
+
+
+
+
+
